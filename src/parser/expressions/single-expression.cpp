2ad0b84ea695ee0141d54a7d5d03dcd8fbbca657
david <davidjmacdonald11@gmail.com>
2023-04-06 15:01:55 -0400

    Reset to basic Kotlin project

--- src/parser/expressions/single-expression.cpp
+++
@@ -1,6 +0,0 @@
-#include "parser/expressions/single-expression.h"
-#include "parser/expressions/conditional-expression.h"
-
-Node* SingleExpression::construct(Parser& parser) {
-    return ConditionalExpression::construct(parser);
-}
